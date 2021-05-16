88df98fdadb01029cfa6049e8bfb7d54ab6fcf88
ajay <ajay8.singh@paytm.com>
2021-05-16 21:07:41 +0530

    Adding loggers

--- functions/auth-fetch/auth-fetch.js
+++ functions/auth-fetch/auth-fetch.js
@@ -1,8 +1,9 @@
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 
 const fetch = require('node-fetch')
-const handler = async () => {
-    const response = await fetch("")
+const handler = async (req, res) => {
+    console.log(req, res)
+    const response = await fetch("http://www.covidreliefresources.space/twitter")
     .then(res => res.json())
     .catch(err => console.error(err));
 
