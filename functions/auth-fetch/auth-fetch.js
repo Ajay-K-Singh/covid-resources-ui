554e6f7795e14a30166a3253268b6fee38816a45
ajay <ajay8.singh@paytm.com>
2021-05-16 21:18:58 +0530

    Fixing Proxy Server

--- functions/auth-fetch/auth-fetch.js
+++ functions/auth-fetch/auth-fetch.js
@@ -1,9 +1,16 @@
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 
 const fetch = require('node-fetch')
-const handler = async (req, res) => {
-    console.log(req, res)
-    const response = await fetch("http://www.covidreliefresources.space/twitter")
+const handler = async (event, context, callback) => {
+    let url = new URL('http://www.covidreliefresources.space/twitter');
+    const { city, resources } = event.queryStringParameters;
+    if (city) {
+      url.searchParams.set('location', city);
+    }
+    if (resources) {
+      url.searchParams.set('resources', resources);
+    }
+    const response = await fetch(url.href)
     .then(res => res.json())
     .catch(err => console.error(err));
 
